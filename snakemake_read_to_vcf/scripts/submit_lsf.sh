a0ea802488cf3fbf877537f7ebb8fab13ec01580
Artem Pankin <artempankin@Artems-MacBook-Pro.local>
2020-03-25 01:31:48 +0100

    250320

--- snakemake_read_to_vcf/scripts/submit_lsf.sh
+++ snakemake_read_to_vcf/scripts/submit_lsf.sh
@@ -1,15 +1,15 @@
-1 #!/usr/bin/env bash
-  2 CLUSTER_CMD=("bsub -q {cluster.queue} -n {cluster.nCPUs} -R {cluster.resourc    es} -M {cluster.memory} -o {cluster.output} -e {cluster.error} -J {cluster.n    ame}")
-  3 #JOB_NAME="$1"
-  4 
-  5 #bsub -R "rusage[mem=1000]" \
-  6 #  -M 1000 \
-  7 #  -o logs/cluster_"$JOB_NAME".o \
-  8 #  -e logs/cluster_"$JOB_NAME".e \
-  9 #  -J "$JOB_NAME" \
- 10   snakemake --cluster-config cluster.yaml \
- 11     --latency-wait 2 \
- 12     --jobs 10 \
- 13     --cluster "${CLUSTER_CMD}"
- 14 
- 15 #exit 0
+#!/usr/bin/env bash
+CLUSTER_CMD=("bsub -q {cluster.queue} -n {cluster.nCPUs} -R {cluster.resourc    es} -M {cluster.memory} -o {cluster.output} -e {cluster.error} -J {cluster.n    ame}")
+ #JOB_NAME="$1"
+ 
+ #bsub -R "rusage[mem=1000]" \
+ #  -M 1000 \
+ #  -o logs/cluster_"$JOB_NAME".o \
+ #  -e logs/cluster_"$JOB_NAME".e \
+ #  -J "$JOB_NAME" \
+snakemake --cluster-config cluster.yaml \
+     --latency-wait 2 \
+     --jobs 10 \
+     --cluster "${CLUSTER_CMD}"
+ 
+#exit 0
