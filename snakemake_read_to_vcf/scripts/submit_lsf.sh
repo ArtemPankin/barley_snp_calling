fa7afe44e86e7d116f76f7aed0f1f1fa5e5e1908
Artem Pankin <artempankin@Artems-MacBook-Pro.local>
2020-03-28 00:20:23 +0100

    snakemake_update_280320

--- snakemake_read_to_vcf/scripts/submit_lsf.sh
+++ snakemake_read_to_vcf/scripts/submit_lsf.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-CLUSTER_CMD=("bsub -q {cluster.queue} -n {cluster.nCPUs} -R {cluster.resourc    es} -M {cluster.memory} -o {cluster.output} -e {cluster.error} -J {cluster.n    ame}")
- #JOB_NAME="$1"
- 
- #bsub -R "rusage[mem=1000]" \
- #  -M 1000 \
- #  -o logs/cluster_"$JOB_NAME".o \
- #  -e logs/cluster_"$JOB_NAME".e \
- #  -J "$JOB_NAME" \
-snakemake --cluster-config cluster.yaml \
-     --latency-wait 2 \
-     --jobs 10 \
-     --cluster "${CLUSTER_CMD}"
- 
-#exit 0
+CLUSTER_CMD=("bsub -q {cluster.queue} -n {cluster.nCPUs} -R {cluster.resources} -M {cluster.memory} -o {cluster.output} -e {cluster.error} -J {cluster.name}")
+#JOB_NAME="$1"
+
+#bsub -R "rusage[mem=1000]" \
+#  -M 1000 \
+#  -o logs/cluster_"$JOB_NAME".o \
+#  -e logs/cluster_"$JOB_NAME".e \
+#  -J "$JOB_NAME" \
+  snakemake --cluster-config cluster.yaml \
+    --rerun-incomplete \
+    --latency-wait 2 \
+    --jobs 10 \
+    --cluster "${CLUSTER_CMD}"
+
+exit 0
